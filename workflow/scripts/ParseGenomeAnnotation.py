8e664fa658f4bc6498f694b0c6628faa20e70e05
RienkHeins <rienkheins@gmail.com>
2023-04-17 15:02:35 +0200

    updated ParseGenomeAnnotation.py, changing unlogical names and adding comments to functions

--- workflow/scripts/ParseGenomeAnnotation.py
+++ workflow/scripts/ParseGenomeAnnotation.py
@@ -6,6 +6,13 @@ sys.stderr = open(snakemake.log[0], 'w')
 
 
 def Parse_input(infile):
+    """
+    Takes the input gff3 file and saves the relevant information
+    to turn the file to a readable CDS annotation.
+    :param infile: Non-converted gff3 file
+    :return: temp4: relevant information for new gff3 file.
+    accession: header information original file.
+    """
     try:
         temp = open(infile, 'r')
         temp2 = temp.readline()
@@ -14,8 +21,8 @@ def Parse_input(infile):
         temp3 = [temp2] + temp.readlines()
         temp.close()
         temp4 = []
-        for i in temp3:
-            temp4.append(i.split('\t'))
+        for line in temp3:
+            temp4.append(line.split('\t'))
         accession = temp4[0][0]
 
     except FileNotFoundError:
@@ -25,14 +32,21 @@ def Parse_input(infile):
 
 
 def Sweep_and_output(input_list, accession, output_file):
+    """
+    Uses old gff3 information and CDS relevant information to create
+    a readable CDS gff3 file.
+    :param input_list: List of lines with old gff3 information
+    :param accession: Header info original gff3 file
+    :param output_file: Name of the output file
+    """
     in_count = len(input_list)
     out_count = 0
     output = open(output_file, 'w')
-    for i in input_list:
+    for line in input_list:
         try:
-            if i[2] == 'CDS':
-                start, end, strand = i[3], i[4], i[6]
-                ID = Get_ID(i[8])
+            if line[2] == 'CDS':
+                start, end, strand = line[3], line[4], line[6]
+                ID = Get_ID(line[8])
                 output.write('\t'.join([accession, 'RefSeq', 'CDS', start, end, '.', strand, '.', ID]) + '\n')
                 out_count += 1
         except IndexError:
@@ -43,21 +57,26 @@ def Sweep_and_output(input_list, accession, output_file):
     return 0
 
 
-def Get_ID(attribute):
-    attributes = attribute.split(';')
+def Get_ID(ID_info):
+    """
+    Gets the line ID from the inputted gff3 line
+    :param ID_info: ID attribute
+    :return: ID from extracted attribute
+    """
+    attributes = ID_info.split(';')
     attributes_dic = {}
-    for i in attributes:
-        attributes_dic[i.split('=')[0]] = i.split('=')[1]
+    for attribute in attributes:
+        attributes_dic[attribute.split('=')[0]] = attribute.split('=')[1]
     return attributes_dic['locus_tag']
 
 
-def main():
-    input = snakemake.input[0]
-    output = snakemake.output[0]
+def main(input, output):
     input_list, accession = Parse_input(input)
     Sweep_and_output(input_list, accession, output)
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    genome_annotation = snakemake.input[0]
+    parsed_annotation = snakemake.output[0]
+    sys.exit(main(genome_annotation, parsed_annotation))
